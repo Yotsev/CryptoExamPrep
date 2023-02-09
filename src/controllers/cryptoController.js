34868360b0aa43db4ef277df684020a360d75b47
Yotsev <b.yotsev@gmail.com>
2023-02-09 11:20:38 +0200

    Views fixed

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -4,5 +4,8 @@ cryptoController.get('/catalog', (req, res)=> {
     res.render('crypto/catalog');
 });
 
+cryptoController.get('/create', (req, res)=> {
+    res.render('crypto/create');
+});
 
 module.exports = cryptoController
