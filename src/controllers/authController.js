ecc300bad6b2c2ca55ea17a43fb1e272e1ab0e74
Yotsev <b.yotsev@gmail.com>
2023-02-09 12:19:39 +0200

    Added register and login functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,36 @@
 const authController = require('express').Router();
+const authService = require('../services/authService');
 
 authController.get('/register', (req, res)=> {
     res.render('auth/register');
 });
 
+authController.post('/register', async (req, res)=> {
+    const {username, email, password, repeatPassword} = req.body;
+
+    await authService.register(username, email, password, repeatPassword);
+
+    res.redirect('/');
+});
+
+
 authController.get('/login', (req, res)=> {
     res.render('auth/login');
 });
+
+authController.post('/login', async (req, res)=> {
+    const {email, password} = req.body;
+
+    const token = await authService.login(email,password);
+
+    res.cookie('auth', token);
+    res.redirect('/');
+});
+
+authController.get('/logout', (req, res)=> {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
+
 
 module.exports = authController;
---
+++ src/services/authService.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const jwt = require('../libs/jsonwebtoken');
+const config = require('../configs/config');
+
+exports.getUserByEmail = (email)=> User.findOne({email});
+
+exports.getExistingUser = async (username, email) => {
+    const existingUser = await User.findOne({$or: [{email}, {username}]});
+    
+    return existingUser;
+};
+
+exports.register = async (username, email, password, repeatPassword) => {
+    const passwordLenght = 4;
+
+    const existingUser = await this.getExistingUser(username, email);
+
+    if (existingUser) {
+        throw new Error('User exists');
+    }
+
+    if (password.length < 4) {
+        throw new Error('Password must be at least 4 characters long');
+    }
+
+    if (password !== repeatPassword) {
+        throw new Error('Password mismatch');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({ username, email, password: hashedPassword });
+};
+
+exports.login = async (email, password) => {
+    const user = await this.getUserByEmail(email);
+
+    if (!user) {
+        throw new Error('Invalid email or password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid email or password');
+    }
+
+    const payload = {
+        _id: user._id,
+        email,
+        username: user.username,
+    };
+
+    const token = await jwt.sign(payload, config.SECRET);
+
+    return token;
+};
