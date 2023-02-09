92e76d55b249164ecfd58e33f0ed03f80dbc9196
Yotsev <b.yotsev@gmail.com>
2023-02-09 13:30:07 +0200

    Added crypto model

---
+++ src/models/Crypto.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const cryptoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    image: {
+        type: String,
+        required: [true, 'Image link is required'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price link is required'],
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['crypto-wallet', 'credit-card', 'debit-card', 'paypal'],
+        required: [true, 'Price link is required'],
+    },
+    buy: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Crypto = mongoose.model('Crypto', cryptoSchema);
+
+module.exports = Crypto;
